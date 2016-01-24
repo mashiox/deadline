47c13811ded04769c3e9efabf082c93f543a3638
Matt Walther <matt@mashio.net>
2016-01-24 13:16:25 -0800

    Implementation of add new task and global clockout

--- client/landingPage/landingPage.js
+++ client/landingPage/landingPage.js
@@ -22,3 +22,37 @@ Template.taskList.events({
     'click a.taskItem': function(event){
     }
 })*/
+
+Template.loggedIn.events({
+    'click button.addTask': function(event){
+        event.preventDefault();
+        // WARNING: INSECURE
+        return Tasks.insert({
+            title: "New Task",
+            desc: "",
+            owner: Meteor.userId(),
+            children: [],
+            clockIn: [],
+            notes: ""
+        });
+    },
+    
+    'click button.clockOut': function(event){
+        event.preventDefault();
+        var tasks = Tasks.find({owner: Meteor.userId()});
+        tasks = tasks.map( function(task){ 
+            task.clockIn = task.clockIn.map(function(clock){
+                clock.punchOut = clock.punchOut === -1 ? new Date() : clock.punchOut;
+                console.log(clock.punchOut);
+                return clock;
+            })
+            return task; 
+        });
+        console.log("whole tasks object:")
+        console.log(tasks);
+        // WARNING: INSECURE
+        tasks.map( function(task){
+            Tasks.update({_id: task._id}, task );
+        });
+    }
+})
